e27b0a2456e674765d8798503e8d87d5283a5794
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 23:40:45 +0530

    Add User

---
+++ AddPersonUC2.cs
@@ -0,0 +1,18 @@
+﻿using AddressBooksystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddPersonUC2
+    {
+        public String firstname, lastname, mobilenumber, address, city, zip, state, email;
+        public override string ToString()
+        {
+            return $"First Name: {firstname}, Last Name: {lastname}, Mobile Number: {mobilenumber}, Address: {address}, City: {city}, State: {state}, Zip: {zip}, Email: {email}";
+        }
+    }   
+}
