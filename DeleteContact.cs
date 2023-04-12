2c0a68997295e08bb0d2fe4d67bcf9e358ee42f5
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-13 00:23:16 +0530

    Delete any user information

---
+++ DeleteContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class DeleteContact
+    {
+        public string firstName;
+        public string lastName;
+        public string mobileNumber;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string email;
+
+        public override string ToString()
+        {
+            return $"Name: {firstName} {lastName}\nMobile Number: {mobileNumber}\nAddress: {address}\nCity: {city}\nState: {state}\nZip: {zip}\nEmail: {email}\n";
+        }
+    }
+}
